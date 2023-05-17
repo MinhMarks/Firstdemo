4d7578df6bcf91baace106951db1d1b6de9f9fb9
Nhat Minh <minhnhat21012005@gmail.com>
2023-05-17 19:50:21 +0700

    gfdgsdf

--- OOP.cpp
+++ OOP.cpp
@@ -4,6 +4,7 @@ class A {public:A() {cout << "Constructing A ";}~A() {cout << "Destructing A ";}
 class B: public A {public:B() {cout << "Constructing B ";}~B() {cout << "Destructing B ";}};
 int main() {
     B b1;
-    print("%s", "fsdsfsdfsf") ; 
+    print("%s", "fsdsfsdfsf") ;
+    while ( true ) ;  
     return 0;
 }
